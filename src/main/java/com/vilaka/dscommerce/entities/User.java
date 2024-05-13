943ca6b3dd6e40104ed1aaea233fa41c059e2700
pativilaka <p.vilaka@gmail.com>
2024-05-13 10:46:39 -0300

    add user class in entities package and lombok lib

---
+++ src/main/java/com/vilaka/dscommerce/entities/User.java
@@ -0,0 +1,32 @@
+package com.vilaka.dscommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String phone, LocalDate birthDate) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.birthDate = birthDate;
+    }
+
+}
