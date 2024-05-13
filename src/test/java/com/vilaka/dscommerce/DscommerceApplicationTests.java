ccf7844418c30c24f18e243e614ebfee98575926
pativilaka <p.vilaka@gmail.com>
2024-05-13 10:14:23 -0300

    add project

---
+++ src/test/java/com/vilaka/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vilaka.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
