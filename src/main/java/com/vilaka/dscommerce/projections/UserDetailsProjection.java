2199bc41f62fdf12a6cc882ff7dcd9b13672a42c
pativilaka <p.vilaka@gmail.com>
2024-05-22 10:36:16 -0300

    add UserDetailService

---
+++ src/main/java/com/vilaka/dscommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,9 @@
+package com.vilaka.dscommerce.projections;
+
+public interface UserDetailsProjection {
+    String getUsername();
+    String getPassword();
+    Long getRoleId();
+    String getAuthority();
+
+}
